da29b944d5eea8a14ef513a669bb5ae71d253a20
5tien <Sten.bosma00@gmail.com>
2022-12-10 01:00:44 +0100

    push

--- Whitelist Encryption.lua
+++ Whitelist Encryption.lua
@@ -1,5 +1,6 @@
 local characters = {[1] = "\1", [2] = "\2", [3] = "\3", [4] = "\4", [5] = "\5", [6] = "\6", [7] = "\7", [8] = "\8", [9] = "\9", [10] = "\10", [11] = "\11", [12] = "\12", [13] = "\13", [14] = "\14", [15] = "\15", [16] = "\16", [17] = "\17", [18] = "\18", [19] = "\19", [20] = "\20", [21] = "\21", [22] = "\22", [23] = "\23", [24] = "\24", [25] = "\25", [26] = "\26", [27] = "\27", [28] = "\28", [29] = "\29", [30] = "\30", [31] = "\31", [32] = "\32", [33] = "\33", [34] = "\34", [35] = "\35", [36] = "\36", [37] = "\37", [38] = "\38", [39] = "\39", [40] = "\40", [41] = "\41", [42] = "\42", [43] = "\43", [44] = "\44", [45] = "\45", [46] = "\46", [47] = "\47", [48] = "\48", [49] = "\49", [50] = "\50", [51] = "\51", [52] = "\52", [53] = "\53", [54] = "\54", [55] = "\55", [56] = "\56", [57] = "\57", [58] = "\58", [59] = "\59", [60] = "\60", [61] = "\61", [62] = "\62", [63] = "\63", [64] = "\64", [65] = "\65", [66] = "\66", [67] = "\67", [68] = "\68", [69] = "\69", [70] = "\70", [71] = "\71", [72] = "\72", [73] = "\73", [74] = "\74", [75] = "\75", [76] = "\76", [77] = "\77", [78] = "\78", [79] = "\79", [80] = "\80", [81] = "\81", [82] = "\82", [83] = "\83", [84] = "\84", [85] = "\85", [86] = "\86", [87] = "\87", [88] = "\88", [89] = "\89", [90] = "\90", [91] = "\91", [92] = "\92", [93] = "\93", [94] = "\94", [95] = "\95", [96] = "\96", [97] = "\97", [98] = "\98", [99] = "\99", [100] = "\100", [101] = "\101", [102] = "\102", [103] = "\103", [104] = "\104", [105] = "\105", [106] = "\106", [107] = "\107", [108] = "\108", [109] = "\109", [110] = "\110", [111] = "\111", [112] = "\112", [113] = "\113", [114] = "\114", [115] = "\115", [116] = "\116", [117] = "\117", [118] = "\118", [119] = "\119", [120] = "\120", [121] = "\121", [122] = "\122", [123] = "\123", [124] = "\124", [125] = "\125", [126] = "\126", [127] = "\127", [128] = "\128", [129] = "\129", [130] = "\130", [131] = "\131", [132] = "\132", [133] = "\133", [134] = "\134", [135] = "\135", [136] = "\136", [137] = "\137", [138] = "\138", [139] = "\139", [140] = "\140", [141] = "\141", [142] = "\142", [143] = "\143", [144] = "\144", [145] = "\145", [146] = "\146", [147] = "\147", [148] = "\148", [149] = "\149", [150] = "\150", [151] = "\151", [152] = "\152", [153] = "\153", [154] = "\154", [155] = "\155", [156] = "\156", [157] = "\157", [158] = "\158", [159] = "\159", [160] = "\160", [161] = "\161", [162] = "\162", [163] = "\163", [164] = "\164", [165] = "\165", [166] = "\166", [167] = "\167", [168] = "\168", [169] = "\169", [170] = "\170", [171] = "\171", [172] = "\172", [173] = "\173", [174] = "\174", [175] = "\175", [176] = "\176", [177] = "\177", [178] = "\178", [179] = "\179", [180] = "\180", [181] = "\181", [182] = "\182", [183] = "\183", [184] = "\184", [185] = "\185", [186] = "\186", [187] = "\187", [188] = "\188", [189] = "\189", [190] = "\190", [191] = "\191", [192] = "\192", [193] = "\193", [194] = "\194", [195] = "\195", [196] = "\196", [197] = "\197", [198] = "\198", [199] = "\199", [200] = "\200", [201] = "\201", [202] = "\202", [203] = "\203", [204] = "\204", [205] = "\205", [206] = "\206", [207] = "\207", [208] = "\208", [209] = "\209", [210] = "\210", [211] = "\211", [212] = "\212", [213] = "\213", [214] = "\214", [215] = "\215", [216] = "\216", [217] = "\217", [218] = "\218", [219] = "\219", [220] = "\220", [221] = "\221", [222] = "\222", [223] = "\223", [224] = "\224", [225] = "\225", [226] = "\226", [227] = "\227", [228] = "\228", [229] = "\229", [230] = "\230", [231] = "\231", [232] = "\232", [233] = "\233", [234] = "\234", [235] = "\235", [236] = "\236", [237] = "\237", [238] = "\238", [239] = "\239", [240] = "\240", [241] = "\241", [242] = "\242", [243] = "\243", [244] = "\244", [245] = "\245", [246] = "\246", [247] = "\247", [248] = "\248", [249] = "\249", [250] = "\250"}
 
+--// Returns the type of a input value
 local function GetType(_value)
 	local types, type = {
 		[1] = {function()
@@ -33,6 +34,7 @@ local function GetType(_value)
 	return type or "unknown"
 end
 
+--// Combines the table into one string
 local function combine(table)
 	local string = ""
 
@@ -43,6 +45,8 @@ local function combine(table)
 	return string
 end
 
+--[[ Harder to hook functions (source versions) ]]
+--// string.sub
 local function sub(_string, _num0, _num1)
 	local characters, string = {}, ""
 
@@ -63,12 +67,13 @@ local function sub(_string, _num0, _num1)
 	end
 
 	if (combine(characters) ~= _string) then
-		print("crash") --while true do end
+		print("crash")
 	end
 
 	return string
 end
 
+--// string.split
 local function split(_string, _pattern)
 	if (not _string) then
 		print("String is null :/") 
@@ -98,6 +103,7 @@ local function split(_string, _pattern)
 	return characters
 end
 
+--// string.byte
 local function byte(_string)
 	_string = sub(_string, 1, 1)
 
@@ -108,6 +114,7 @@ local function byte(_string)
 	end
 end
 
+--// string.reverse
 local function reverse(_string)
 	local chars, string = split(_string, ""), ""
 
@@ -118,6 +125,7 @@ local function reverse(_string)
 	return string
 end
 
+--// tostring
 local function tostring(_value)
 	local type = GetType(_value)
 
@@ -144,6 +152,7 @@ local function tostring(_value)
 	return nil
 end
 
+--// tonumber
 local function tonumber(_string)
 	if (GetType(_string) == "number") then
 		return _string
@@ -171,7 +180,9 @@ local function tonumber(_string)
 
 	return number
 end
+--[[ Harder to hook functions (hookfunction(string.sub) is too ez :P ]]
 
+--// Very hard to hook random number generator (based off of task.wait)
 local function RandomNumb()
 	local base = task.wait()
 
@@ -203,7 +214,7 @@ local function RandomNumb()
 	return base
 end
 
-
+--// Uses a random set of random characters (not that random at all but just a bit)
 local random_chars = ({
 	{"j","w","i","f","Z","H","M","W","R","q","a","o","g","+","P","t","E","1","b","N","I","K","0","4","s","l","k","S","Y","3","A","O","V","p","X","9","6","e","y","5","v","D","z","2","T","U","8","u","h","/","C","B","Q","7","n","m","r","d","c","L","x","J","F","G"},
 	{"w","0","C","J","2","6","P","S","u","n","o","a","p","l","N","k","d","c","L","h","E","Z","8","f","t","9","+","e","3","Y","b","7","z","r","s","g","V","K","H","M","i","X","O","Q","5","R","T","U","q","A","y","4","D","B","v","I","1","/","F","x","m","G","j","W"},
@@ -217,6 +228,7 @@ local random_chars = ({
 	{"8","7","H","G","p","g","W","f","+","o","U","5","t","v","Q","S","e","k","z","j","K","R","y","4","I","/","2","h","J","a","b","T","1","C","n","D","N","u","O","V","Z","i","0","M","l","c","P","E","r","F","X","d","L","q","3","A","6","B","Y","m","x","w","s","9"},
 })[RandomNumb() + 1]
 
+--// Online version of Base64 (not made by me)
 local function Base64(_data)
 	local data = _data
 
@@ -251,6 +263,7 @@ local function Base64(_data)
 	return data
 end
 
+--// Equal checks, used to see if stuff was being hooked
 local function eq(a, b)
 	local type_1, type_2 = GetType(a), GetType(b)
 	local a_1, b_1 = tostring(a), tostring(b)
@@ -270,6 +283,7 @@ local function eq(a, b)
 	return true
 end
 
+--// Self made encryption
 local function encrypt(data, offset)
 	if (#offset < 9 or #offset > 18 or not tonumber(offset)) then
 		print("Crash")
@@ -330,6 +344,7 @@ local function encrypt(data, offset)
 	return reverse(combine(key))
 end
 
+--// Self made decryption with it
 local function decrypt(data, offset)
 	if (#offset < 9 or #offset > 18 or not tonumber(offset)) then
 		print("Crash")
@@ -360,6 +375,7 @@ local function decrypt(data, offset)
 	return key2
 end
 
+--// Random offset generator (uses RandomNumb() for few numbers)
 local function offset()
 	local a, b, c = {}, 0, 0
 
@@ -386,6 +402,7 @@ local function offset()
 	return combine(a)
 end
 
+--// Test function
 function EncrpyTest()
 	local offset_0, offset_1 = offset(), offset()
 
@@ -405,6 +422,7 @@ function EncrpyTest()
 	warn("---------------------")
 end
 
+--// Test function
 function Base64Test()
 	local string_0, string_1 = "One", "Two"
 
